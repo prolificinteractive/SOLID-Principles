e3d069a902211b6557013672886d4ee79e2beb38
Harlan Kellaway <harlan@prolificinteractive.com>
2016-01-12 13:15:47 -0500

    Created non-SOLID example files

---
+++ SOLIDExample/SOLIDExample/TwitterExample/NotSOLID/NotSOLIDProfileService.swift
@@ -0,0 +1,17 @@
+//
+//  NotSOLIDProfileService.swift
+//  SOLIDExample
+//
+//  Created by Harlan Kellaway on 1/12/16.
+//  Copyright © 2016 Prolific Interactive. All rights reserved.
+//
+
+import Foundation
+
+struct NotSOLIDProfileService {
+    
+    static func fetchProfileForUserID(userID: String) {
+        print("fetched profile")
+    }
+    
+}
---
+++ SOLIDExample/SOLIDExample/TwitterExample/NotSOLID/NotSOLIDServiceScheduler.swift
@@ -0,0 +1,55 @@
+//
+//  NotSOLIDServiceScheduler.swift
+//  SOLIDExample
+//
+//  Created by Harlan Kellaway on 1/12/16.
+//  Copyright © 2016 Prolific Interactive. All rights reserved.
+//
+
+import Foundation
+
+class NotSOLIDServiceScheduler: NSObject {
+    
+    private var serviceTimer: NSTimer? = nil
+    private var timerTick: Int = 0
+    
+    // MARK: - Protocol conformance
+    
+    // MARK: ServiceScheduler
+    
+    func start() {
+        // starts timer
+        self.serviceTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "timerDidFire:", userInfo: nil, repeats: true)
+    }
+    
+    func stop() {
+        self.serviceTimer?.invalidate()
+        self.serviceTimer = nil
+    }
+    
+    // MARK: - Private methods
+    
+    private func timerDidFire(timer: NSTimer) {
+        self.timerTick++
+        self.runServicesWithTick(self.timerTick)
+    }
+    
+    private func runServicesWithTick(tick: Int) {
+        // decides frequencies
+        if ((tick % 1) == 0) {
+            self.runTimelineUpdateService()
+        } else if ((tick % 2) == 0) {
+            self.runProfileUpdateService()
+        }
+    }
+    
+    // executes services
+    private func runTimelineUpdateService() {
+        NotSOLIDTimelineService.fetchTimelineForUserID("123")
+    }
+    
+    private func runProfileUpdateService() {
+        NotSOLIDProfileService.fetchProfileForUserID("123")
+    }
+    
+}
